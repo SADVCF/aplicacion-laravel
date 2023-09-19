74fffe86cd45d9ec15150582e09be8aa60e7b4a9
JoseAntonio <jose.antonio_pg@hotmail.com>
2023-09-19 04:31:00 +0200

    Principio del desarrollo del layout principal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,13 +19,29 @@
                 DevsTagram
             </h1>
 
-            <nav class="flex gap-5">
+            <nav class="flex gap-2 items-center">
                 <a href class="font-bold uppercase text-gray-600 text-sm"="">Login</a>
                 <a href class="font-bold uppercase text-gray-600 text-sm"="">Crear cuenta</a>
             </nav>
         </div>
         
         </header>
+
+        <main class="container mx-auto mt-10 ">
+            <h2 class="font-black text-center text-3xl mb-10">
+                @yield('titulo')
+            </h2>
+                @yield('contenido')
+        </main>
+
+        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+            DevStagram - Todos los derechos reservados
+            {{now()->year}}
+                
+            
+        </footer>
+       
+        
         
     
 
